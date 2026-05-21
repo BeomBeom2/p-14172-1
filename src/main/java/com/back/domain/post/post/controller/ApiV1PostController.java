349bb94afc2fad48a90d94d6242057523e9998b8
BeomBeom2 <whwjdqja20@naver.com>
2026-05-21 09:12:24 +0900

    18강

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -40,7 +40,7 @@ public class ApiV1PostController {
 
     @GetMapping("/{id}/delete")
     @Transactional
-    public RsData delete(@PathVariable int id) {
+    public RsData<PostDto> delete(@PathVariable int id) {
         Post post = postService.findById(id).get();
 
         postService.delete(post);
