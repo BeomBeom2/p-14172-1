de2588fb78c1d39807b96b69bc1ef40a9a904a4f
BeomBeom2 <whwjdqja20@naver.com>
2026-05-21 09:10:57 +0900

    17강

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -1,9 +1,10 @@
-package com.back.domain.post.post.controller;
+package com.back.domain.post.postComment.controller;
 
-import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
-import com.back.global.rsData.ForPostRsData;
+import com.back.domain.post.postComment.dto.PostCommentDto;
+import com.back.domain.post.postComment.entity.PostComment;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,41 +15,54 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/posts")
+@RequestMapping("/api/v1/posts/{postId}/comments")
 @RequiredArgsConstructor
 public class ApiV1PostCommentController {
     private final PostService postService;
 
     @GetMapping
     @Transactional(readOnly = true)
-    public List<PostDto> getItems() {
-        List<Post> items = postService.findAll();
+    public List<PostCommentDto> getItems(
+            @PathVariable int postId
+    ) {
+        Post post = postService.findById(postId).get();
 
-        return items
+        return post
+                .getComments()
                 .stream()
-                .map(PostDto::new) // PostDto로 변환
+                .map(PostCommentDto::new)
                 .toList();
     }
 
     @GetMapping("/{id}")
     @Transactional(readOnly = true)
-    public PostDto getItem(@PathVariable int id) {
-        Post post = postService.findById(id).get();
+    public PostCommentDto getItem(
+            @PathVariable int postId,
+            @PathVariable int id
+    ) {
+        Post post = postService.findById(postId).get();
 
-        return new PostDto(post);
+        PostComment postComment = post.findCommentById(id).get();
+
+        return new PostCommentDto(postComment);
     }
 
     @GetMapping("/{id}/delete")
     @Transactional
-    public ForPostRsData delete(@PathVariable int id) {
-        Post post = postService.findById(id).get();
+    public RsData delete(
+            @PathVariable int postId,
+            @PathVariable int id
+    ) {
+        Post post = postService.findById(postId).get();
 
-        postService.delete(post);
+        PostComment postComment = post.findCommentById(id).get();
 
-        return new ForPostRsData(
+        postService.deleteComment(post, postComment);
+
+        return new RsData(
                 "200-1",
-                "%d번 글이 삭제되었습니다.".formatted(id),
-                new PostDto(post)
+                "%d번 댓글이 삭제되었습니다.".formatted(id),
+                new PostCommentDto(postComment)
         );
     }
 }
