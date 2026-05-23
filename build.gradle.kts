5885dd1812e2dc7c936aa1b155592825997a3a8d
BeomBeom2 <whwjdqja20@naver.com>
2026-05-23 18:53:53 +0900

    32강

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,21 +19,21 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-h2console")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
-    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
-    testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testAnnotationProcessor("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -70,8 +70,8 @@ public class ApiV1PostController {
 
     @PostMapping
     @Transactional
-    public RsData<PostWriteResBody> write(@Valid @RequestBody PostWriteReqBody form) {
-        Post post = postService.write(form.title, form.content);
+    public RsData<PostWriteResBody> write(@Valid @RequestBody PostWriteReqBody reqBody) {
+        Post post = postService.write(reqBody.title, reqBody.content);
 
         return new RsData<>(
                 "201-1",
@@ -83,20 +83,28 @@ public class ApiV1PostController {
         );
     }
 
-    @PatchMapping("/{id}")
+    record PostModifyReqBody(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String title,
+            @NotBlank
+            @Size(min = 2, max = 5000)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
     @Transactional
-    public RsData<PostWriteResBody> update(
+    public RsData<Void> modify(
             @PathVariable int id,
-            @Valid @RequestBody PostWriteReqBody form) {
-         Post post = postService.modify(postService.findById(id).get(), form.title, form.content);
+            @Valid @RequestBody PostModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(id).get();
+        postService.modify(post, reqBody.title, reqBody.content);
 
         return new RsData<>(
                 "200-1",
-                "%d번 글이 수정되었습니다.".formatted(post.getId()),
-                new PostWriteResBody(
-                        postService.count(),
-                        new PostDto(post)
-                )
+                "%d번 글이 수정되었습니다.".formatted(post.getId())
         );
     }
 }
