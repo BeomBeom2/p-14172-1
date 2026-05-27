e94ddf2b579f1bbfd88689b88701f4a3eb68f3fc
BeomBeom2 <whwjdqja20@naver.com>
2026-05-27 09:46:59 +0900

    61강

--- src/main/java/com/back/global/springDoc/SpringDocConfig.java
+++ src/main/java/com/back/global/springDoc/SpringDocConfig.java
@@ -7,4 +7,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "API 서버", version = "beta", description = "API 서버 문서입니다."))
 public class SpringDocConfig {
+    @Bean
+    public GroupedOpenApi groupApiV1() {
+        return GroupedOpenApi.builder()
+                .group("apiV1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupController() {
+        return GroupedOpenApi.builder()
+                .group("home")
+                .pathsToExclude("/api/**")
+                .build();
+    }
 }
